a237f166af35fe6093b64a651e3085c33bd59227
Mohammad Afzal Khan <afzalkhan7868687@gmail.com>
2026-01-30 12:47:20 +0530

    Create static3.java

---
+++ static3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class static2 {
+    //  static int n;
+    // int n;
+     public static void main(String [] args){
+        Scanner s = new Scanner (System.in);
+        // static2 m = new static2();
+        int n;
+        // m.n = s.nextInt()
+        n = s.nextInt();
+        System.out.print(n);
+        // System.out.print(m.n);
+    }
+}
