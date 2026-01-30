f4e8d43b3d0d936801615f98482a35e649f2866e
Mohammad Afzal Khan <afzalkhan7868687@gmail.com>
2026-01-30 11:28:49 +0530

    Create lab1_code4.java

---
+++ lab1_code4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class lab1_code4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int sum = 0;
+
+        System.out.println("Enter array elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            sum = sum + arr[i];
+        }
+
+        System.out.println("Sum of array = " + sum);
+    }
+}
