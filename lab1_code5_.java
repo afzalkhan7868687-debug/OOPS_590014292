65d3450c802dab2db646627b4a47311703732ee9
Mohammad Afzal Khan <afzalkhan7868687@gmail.com>
2026-01-30 11:08:32 +0530

    Create lab1_code5_.java

---
+++ lab1_code5_.java
@@ -0,0 +1,16 @@
+public class lab1_code5_{
+
+    // Variables declared outside main (instance variables)
+    int roll = 105;
+    double marks = 98.5;
+    String course = "CSE";
+
+    public static void main(String[] args) {
+
+        // Create object of Student class
+        lab1_code5_ s = new lab1_code5_();
+        System.out.println("Roll No: " + s.roll);
+        System.out.println("Marks: " + s.marks);
+        System.out.println("Course: " + s.course);
+    }
+}
