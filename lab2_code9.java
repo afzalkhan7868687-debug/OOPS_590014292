15c77fcdfb6d749ad8abd9e5d6811af542db33a6
Mohammad Afzal Khan <afzalkhan7868687@gmail.com>
2026-01-31 08:30:38 +0530

    Create lab2_code9.java

---
+++ lab2_code9.java
@@ -0,0 +1,13 @@
+class Student {
+
+    Student() {   // Constructor
+        System.out.println("Constructor called: Student object created");
+    }
+}
+
+class lab2_code9 {
+    public static void main(String[] args) {
+
+        Student s1 = new Student();   // Constructor invoked here
+    }
+}
